6e8de0c9711e5708997190167e01240399d1afb6
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-02-04 21:59:50 -0600

    Initial commit

---
+++ Cartesian/src/Run.java
@@ -0,0 +1,12 @@
+public class Run {
+	public static void main(String[] args) {
+		XYCoordinate myFirstCoordinate = new XYCoordinate();
+		myFirstCoordinate.x = 5;
+		myFirstCoordinate.y = 7;
+		myFirstCoordinate.print();
+		XYCoordinate mySecondCoordinate = new XYCoordinate();
+		mySecondCoordinate.x = 8;
+		mySecondCoordinate.y = 19;
+		mySecondCoordinate.print();
+	}
+}
---
+++ Cartesian/src/XYCoordinate.java
@@ -0,0 +1,8 @@
+public class XYCoordinate {
+	public int x;
+	public int y;
+	
+	public void print() {
+		System.out.println("(" + x + ", " + y + ")");
+	}
+}
